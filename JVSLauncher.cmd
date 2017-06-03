e47bf6657f14428b3bd6d2365a8f739ea57e5e81
WeirdyBenji <benji.the.weird@gmail.com>
2017-06-03 20:25:45 +0200

    Final
    Helps to start javascool-proglets.jar if user have issues

---
+++ JVSLauncher.cmd
@@ -0,0 +1,6 @@
+@echo off
+title Jarfiles Opener
+echo Loading...
+echo If you close this window, your software will be closed too.
+echo Si vous quittez cette fenetre, l'application lancee se quittera egalement.
+java -jar javascool-proglets.jar
